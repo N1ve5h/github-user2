19bf048ecd6a9ed14abfd66fd8d52d7e318fced3
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-11-29 14:02:12 +0000

    Save username using state

--- src/Components/SearchUser.js
+++ src/Components/SearchUser.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { User } from "./User";
+import axios from "axios";
 
 export const SearchUser = () => {
+  const [username, setUsername] = useState('');
   return (
     <main>
-      <form class="search">
-        <label class="search__image" for="search">
+      <form className="search">
+        <label className="search__image" htmlFor="search">
           <img src="./assets/icon-search.svg" alt="" />
         </label>
         <input
-          class="search__input"
+          required
+          className="search__input"
           type="text"
           name="searchgituser"
           id="searchgituser"
-          placeholder="Search Github Username..."
+          placeholder="Search Github Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <button class="search__button">Search</button>
+        <button className="search__button">Search</button>
+
       </form>
+      <User username={username}/>
     </main>
   );
 };
