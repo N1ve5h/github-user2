7e0170be62d1e09abb15cf45d9ec6e4a8896a064
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-11-29 14:01:30 +0000

    color style

--- src/themes.js
+++ src/themes.js
@@ -2,17 +2,42 @@ import { createGlobalStyle } from "styled-components";
 
 export const lightTheme = {
   body: "#F5F8FF",
-  fontColor: "#000",
+  headerFontColor: "#000",
+  backgroundColor: "white",
+  shadow: "0px 0px 10px #c2c2c2",
+  autofillColor: "black",
 };
 
 export const darkTheme = {
   body: "#141C2F",
-  fontColor: "#fff",
+  headerFontColor: "#fff",
+  backgroundColor: "#1f2a48",
+  shadow: "none",
+  autofillColor: "white",
 };
 
 export const GlobalStyle =  createGlobalStyle`
 body {
   background-color: ${props => props.theme.body};
-  color: ${props => props.theme.fontColor};
+}
+
+.header {
+  color: ${props => props.theme.headerFontColor};
+}
+
+.search {
+  background-color: ${props => props.theme.backgroundColor};
+  box-shadow: ${props => props.theme.shadow};
+
+  &__input {
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.headerFontColor};
+    &::placeholder {
+    color: ${props => props.theme.headerFontColor};
+    }
+    &:-webkit-autofill {
+      -webkit-text-fill-color: ${props => props.theme.autofillColor};
+    }
+  }
 }
 `
